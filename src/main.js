1f366034c1249c3a3638c602da0bf0baa7a4f3da
antoni andre <antoniandre.web@gmail.com>
2021-05-24 20:31:20 -0400

    Register all global components and add vue router.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './app.vue'
 import WaveUI from 'wave-ui'
 import 'wave-ui/dist/wave-ui.css'
+import router from './router'
+
+import SshPre from 'simple-syntax-highlighter'
+import 'simple-syntax-highlighter/dist/sshpre.css'
+import TitleLink from '@/documentation/components/title-link.vue'
+import IssueLink from '@/documentation/components/issue-link.vue'
+import UiComponentTitle from '@/documentation/components/ui-component-title.vue'
+import Example from '@/documentation/components/example.vue'
+import Alert from '@/documentation/components/alert.vue'
+import Api from '@/documentation/components/api.vue'
 
 const app = createApp(App)
+app.use(router)
 
 new WaveUI(app, {
   // Some Wave UI options.
 })
+
+app.component('ssh-pre', SshPre)
+app.component('title-link', TitleLink)
+app.component('ui-component-title', UiComponentTitle)
+app.component('example', Example)
+app.component('alert', Alert)
+app.component('component-api', Api) // The `api` keyword in template crashes Vue 3.
+app.component('issue-link', IssueLink)
 
 app.mount('#app')
