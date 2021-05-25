6a1ec4b38ca4b587f9c18ae19a09e9c0a0b96b5e
antoni andre <antoniandre.web@gmail.com>
2021-05-24 20:33:18 -0400

    Resolve fiel paths starting with `@`.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from "path"
 
 export default defineConfig({
   plugins: [vue()], // https://vitejs.dev/config/
   css: {
     preprocessorOptions: {
       scss: { additionalData: `@import "./src/wave-ui/scss/variables";@import "@/documentation/scss/_variables.scss";` }
+    }
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "/src")
     }
   }
 })
