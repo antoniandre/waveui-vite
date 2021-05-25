e46dcfb17ff1676b6b553aaf99058dea5b7f0c08
antoni andre <antoniandre.web@gmail.com>
2021-05-24 20:31:45 -0400

    Add SCSS variables globally.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()], // https://vitejs.dev/config/
+  css: {
+    preprocessorOptions: {
+      scss: { additionalData: `@import "./src/wave-ui/scss/variables";@import "@/documentation/scss/_variables.scss";` }
+    }
+  }
 })
