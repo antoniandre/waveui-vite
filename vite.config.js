7d205e37fc200f94b531744d5e9b95faff13831f
antoni andre <antoniandre.web@gmail.com>
2021-05-30 17:52:08 -0400

    Update build & build-bundle scripts & config.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,31 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
+import del from 'rollup-plugin-delete'
+
+const build = process.env.BUNDLE ? {
+  lib: {
+    entry: resolve(__dirname, '/src/wave-ui/index.js'),
+    name: 'WaveUI'
+  },
+  rollupOptions: {
+    plugins: [
+      del({ targets: ['dist/{images,.htaccess,ghspa.js}', 'dist/*.{ico,txt,html}'], hook: 'generateBundle' })
+    ],
+    // make sure to externalize deps that shouldn't be bundled
+    // into your library
+    external: ['vue'],
+    output: {
+      // Provide global variables to use in the UMD build
+      // for externalized deps
+      globals: {
+        vue: 'Vue'
+      }
+    }
+  }
+} : {
+  outDir: 'docs'
+}
 
 export default defineConfig({
   define: {
@@ -21,7 +46,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/wave-ui/scss/variables";@import "@/documentation/scss/_variables.scss";`
+        additionalData: `@import "@/wave-ui/scss/variables";@import "@/documentation/scss/_variables.scss";`
       }
     }
   },
@@ -30,22 +55,5 @@ export default defineConfig({
       '@': resolve(__dirname, '/src')
     }
   },
-  build: {
-    lib: {
-      entry: resolve(__dirname, './src/wave-ui/index.js'),
-      name: 'WaveUI'
-    },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ['vue'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    }
-  }
+  build
 })
